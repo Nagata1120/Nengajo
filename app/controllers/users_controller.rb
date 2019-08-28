423cbb37e06e9744ccb1e2c26a49d9b63b3ed8f4
Nagata Daiki <d1630@matsue-ct.jp>
2019-08-28 13:05:28 +0900

    Add users 8/28

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,9 +26,8 @@ class UsersController < ApplicationController
 
   # POST /users
   # POST /users.json
-  def  created
+  def create
     @user = User.new(user_params)
-
       respond_to do |format|
       if @user.save 
         format.html { redirect_to @user, notice: '登録されました' }
