57ca0c3f1a1ad6bace190e24ec795960c4e5d5d9
Nagata1120 <54344669+Nagata1120@users.noreply.github.com>
2019-08-29 10:56:19 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get 'session/new'
-=======
-
   get 'session/new'
 
->>>>>>> 015d78598c0196db9460b1ada077d8efe4066dd8
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'routes#index'
@@ -15,11 +11,6 @@ Rails.application.routes.draw do
   get  'menu/:id'  => 'menu#index'        #idをindex(メニュー画面)に渡す
   root to: 'session#index'
   get 'menu/index' 
-<<<<<<< HEAD
+
 end
 
-
-
-=======
-end
->>>>>>> 015d78598c0196db9460b1ada077d8efe4066dd8
